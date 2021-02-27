ff5ce4e974c45c5982d03add2fcc7171168b3722
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-27 20:46:32 -0300

    creating mock to pizzaSize

---
+++ backend/src/mocks/pizzaSizes.ts
@@ -0,0 +1,10 @@
+import PizzaSize from "../entities/PizzaSize";
+
+const pizzaSizes = [
+    new PizzaSize({id: 1, name: 'Brotinho', description: '4 pedaços'}),
+    new PizzaSize({id: 2, name: 'Média', description: '8 pedaços'}),
+    new PizzaSize({id: 3, name: 'Grande', description: '12 pedaços'}),
+    new PizzaSize({id: 4, name: 'Gigante', description: '16 pedaços'}),
+];
+
+export default pizzaSizes;
