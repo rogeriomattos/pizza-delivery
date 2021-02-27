47b2ca4d4499ab9110f4bd5d571aeefb2e5ddb60
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-27 20:49:17 -0300

    creating route to pizza sizes

--- backend/src/routes/pizzas.routes.ts
+++ backend/src/routes/pizzas.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import pizzas from '../mocks/pizzas';
+import pizzaSizes from '../mocks/pizzaSizes';
+
 const pizzasRouter = Router();
 
 pizzasRouter.get('/', (request, response) => {
     response.json(pizzas);
+});
+
+pizzasRouter.get('/sizes', (request, response) => {
+    response.json(pizzaSizes);
 });
 
 export default pizzasRouter;
