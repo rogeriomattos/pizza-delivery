4868ccec5bced1ac9a6525f12fe7c59ec80f5bdb
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-27 21:13:31 -0300

    Adding the property to the percentage of the pizza price relative to the size of the pizza

--- backend/src/entities/PizzaSize.ts
+++ backend/src/entities/PizzaSize.ts
@@ -2,11 +2,13 @@ class PizzaSize {
     id: number;
     name: string;
     description: string;
+    percentageOfPrice: number;
 
-    constructor({ id, name, description }: PizzaSize) {
+    constructor({ id, name, description, percentageOfPrice }: PizzaSize) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.percentageOfPrice = percentageOfPrice;
     }
 }
 
--- backend/src/mocks/pizzaSizes.ts
+++ backend/src/mocks/pizzaSizes.ts
@@ -1,10 +1,10 @@
 import PizzaSize from "../entities/PizzaSize";
 
 const pizzaSizes = [
-    new PizzaSize({id: 1, name: 'Brotinho', description: '4 pedaços'}),
-    new PizzaSize({id: 2, name: 'Média', description: '8 pedaços'}),
-    new PizzaSize({id: 3, name: 'Grande', description: '12 pedaços'}),
-    new PizzaSize({id: 4, name: 'Gigante', description: '16 pedaços'}),
+    new PizzaSize({id: 1, name: 'Brotinho', description: '4 pedaços', percentageOfPrice: 50 }),
+    new PizzaSize({id: 2, name: 'Média', description: '8 pedaços', percentageOfPrice: 100 }),
+    new PizzaSize({id: 3, name: 'Grande', description: '12 pedaços', percentageOfPrice: 125 }),
+    new PizzaSize({id: 4, name: 'Gigante', description: '16 pedaços', percentageOfPrice: 150 }),
 ];
 
 export default pizzaSizes;
