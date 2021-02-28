7d1e1d5497a735a658f8d478a9130e8240f9cfd8
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-27 21:05:17 -0300

    Creating PizzaDugh

---
+++ backend/src/entities/PizzaDough.ts
@@ -0,0 +1,11 @@
+class PizzaDough {
+    id: number;
+    name: string;
+
+    constructor({ id, name }: PizzaDough) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+export default PizzaDough;
