dabd71bc7ca30e4280a91dd29b2225c7797c7cd6
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-28 14:11:06 -0300

    Receiving recommendation of Day

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles.css';
-import Footer from '../../components/Footer';
-import Header from '../../components/Header';
+import formatPrice from '../../utils/formatPrice';
+
 import Recommendation from '../../components/Recommendation';
+import api from '../../services/api';
 
 const Home = () => {
+    const [dailyRecommendation, setDailyRecommendation] = useState({
+        discount: 0,
+        pizza:{name: "", price: 0}
+    });
+
+    useEffect(()=>{
+        api.get('/promotions/today')
+        .then((res) => {
+            setDailyRecommendation(res.data);
+        });
+    }, []);
+
     return ( 
         <div id="page-home">
+            {dailyRecommendation != undefined &&
             <Recommendation
-                title={'Recomendação do dia'}
-                description={'Pizza de Muçarela'}
+                title={'Recomendação do dia ' + dailyRecommendation.discount+'% off'}
+                description={dailyRecommendation.pizza.name}
                 buttonText={'Pedir'}
                 buttonLinkTo={'/Address'}
-            /> 
+            /> }
             <Recommendation
                 title={'Monte a pizza do seu jeito'}
                 description={'Varios Sabores exclusivos para você'}
