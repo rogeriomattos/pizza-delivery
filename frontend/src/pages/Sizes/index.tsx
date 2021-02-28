d12ffd2fc72522b07fad59287a50799919a2ef65
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-28 15:20:21 -0300

    Page Sizes

--- frontend/src/pages/Sizes/index.tsx
+++ frontend/src/pages/Sizes/index.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Radio from '../../components/Radio';
+import IOrder from '../../entites/IOrder';
+import ISize from '../../entites/ISize';
+import api from '../../services/api';
 import './styles.css';
+import { FiArrowRight } from 'react-icons/fi';
+import StepActions from '../../components/StepActions';
+
 
 const Sizes = () => {
+    const [pizzaSizes, setPizzaSizes] = useState<ISize[]>([]);
+    const [order, setOrder] = useState<IOrder>({
+        size: undefined
+    });
+
+    useEffect(() => {
+        api.get('/pizzas/sizes').then((res)=>{
+            console.log(res.data); 
+            setPizzaSizes(res.data);
+        });
+    }, []);
+
     return (
-        <div>Sizes</div>
+        <div>
+            <h2>Escolha o tamanho</h2>
+            {pizzaSizes.map((item) => (
+                <Radio
+                    key={item.id}
+                    name={'sizes'}
+                    title={item.name}
+                    description={item.description}
+                    data={item}
+                    onClick={(data) => {
+                        setOrder({...order, size: data});
+                    }}
+                />
+            ))}
+
+            <StepActions 
+                backLink={'/'}
+                nextLink={'/Daughs'}
+                nextText={'Escolher Massa'}
+                order={order}
+            />
+        </div>
     );  
 };
 
