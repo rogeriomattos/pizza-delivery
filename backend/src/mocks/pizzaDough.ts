80445dac11dd5470f7dc61714946a74ea398c35c
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-27 21:05:32 -0300

    Creating pizza dough mock

---
+++ backend/src/mocks/pizzaDough.ts
@@ -0,0 +1,9 @@
+import PizzaDough from "../entities/PizzaDough";
+
+const pizzaDough: PizzaDough[] = [
+    new PizzaDough({id: 1, name: 'Fina'}),
+    new PizzaDough({id: 2, name: 'Tradicional'}),
+    new PizzaDough({id: 3, name: 'Pan'})
+];
+
+export default pizzaDough;
