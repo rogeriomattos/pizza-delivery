db3612bec4fe42ab8d8afa6d135a5ed531e2edff
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-28 13:18:54 -0300

    Home struct

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './styles.css';
-
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import Recommendation from '../../components/Recommendation';
+ 
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div id="page-home">
+            <Recommendation
+                title={'Recomendação do dia'}
+                description={'Pizza de Muçarela'}
+                buttonText={'Pedir'}
+            />
+            <Recommendation
+                title={'Monte a pizza do seu jeito'}
+                description={'Varios Sabores exclusivos para você'}
+                buttonText={'Ver Cardápio'}
+            />
+        </div>
+    );
 };
 
 export default Home;
