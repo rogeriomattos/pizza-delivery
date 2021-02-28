dad1830fc87154c84cf8102391ef36f772de0068
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-02-28 15:16:03 -0300

    add entites

---
+++ frontend/src/entites/IOrder.ts
@@ -0,0 +1,7 @@
+import ISize from "./ISize";
+
+interface IOrder {
+    size?: ISize;
+};
+
+export default IOrder;
---
+++ frontend/src/entites/ISize.ts
@@ -0,0 +1,7 @@
+interface ISize {
+    id: number;
+    name: string;
+    description: string;
+}
+
+export default ISize;
